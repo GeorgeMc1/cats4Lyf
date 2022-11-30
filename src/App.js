d4ebd169601b1c24602b1257011b7b4413f17693
GeorgeMc1 <george.man1@hotmail.co.uk>
2022-11-30 11:19:13 +0000

    started home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
+import {Home} from "./Pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-      
+
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
