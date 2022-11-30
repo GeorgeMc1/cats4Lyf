f4b38e70c04d344e2cda80f6f5e8bdc87465c4bb
GeorgeMc1 <george.man1@hotmail.co.uk>
2022-11-30 15:31:58 +0000

    putting states in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {useState, useEffect} from "react";
+import { faker } from '@faker-js/faker';
 import './App.css';
 import {Home} from "./Pages/Home";
 
 function App() {
+  const [fullCatList, setFullCatList] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
+        if (!response.ok){ 
+          throw new Error(response.statusText);
+        }
+        const converted = await response.json();
+        setFullCatList(converted);
+      } catch (err){
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, [])
+  fullCatList.forEach(cat => {
+    cat.name = faker.name.firstName();
+    cat.breed = faker.animal.cat();
+    cat.price = faker.commerce.price(100, 500, 0);
+  });
   return (
     <BrowserRouter>
 
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home catList={fullCatList}/>}/>
       </Routes>
     </BrowserRouter>
   );
