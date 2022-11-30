84c7589bf91a4501e8dc2c4a2cd8cd9faa2e8bd7
GeorgeMc1 <george.man1@hotmail.co.uk>
2022-11-30 11:16:07 +0000

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       
-    </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
